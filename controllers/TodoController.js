ba88c7934b3bc6a57f8d8a2a0bd2f2ebadfa83b5
fulviocanducci <fulviocanducci@hotmail.com>
2020-05-27 19:23:29 -0300

    TodoController

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -3,7 +3,6 @@ const { query, save } = require("../connection");
 module.exports = {
   get: function (req, res) {
     query("SELECT * FROM todos", function (error, result, field) {
-      console.log(result);
       if (error) {
         res.json(error);
       } else {
@@ -13,9 +12,20 @@ module.exports = {
   },
   post: function (req, res) {
     const params = req.body;
-    console.log(params);
+    save("INSERT INTO todos SET ?", params, function (error, result, field) {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json(result);
+      }
+    });
+  },
+  put: function (req, res) {
+    const { description, done } = req.body;
+    const { id } = req.params;
+    const params = [description, done, id];
     save(
-      "INSERT INTO todos(`description`,`done`) VALUES(?,?)",
+      "UPDATE todos SET description = ?, done = ? WHERE id=?",
       params,
       function (error, result, field) {
         if (error) {
@@ -25,5 +35,20 @@ module.exports = {
         }
       }
     );
+  },
+  delete: function (req, res) {
+    const { id } = req.params;
+    const params = [id];
+    save("DELETE FROM todos WHERE id=?", params, function (
+      error,
+      result,
+      field
+    ) {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json(result);
+      }
+    });
   },
 };
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var express = require("express");
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const TodoController = require("./controllers/TodoController");
 
-app.get("/", function (req, res) {
-  res.send("Olá Mundo!");
-});
-
+// api get todo ...
 app.get("/todo", TodoController.get);
+app.post("/todo", TodoController.post);
+app.put("/todo/:id", TodoController.put);
+app.delete("/todo/:id", TodoController.delete);
 
+// init service port 3000 ...
+app.get("/", function (req, res) {
+  res.send({ status: true, description: "Servidor Online" });
+});
 app.listen(3000, function () {
   console.log("App de Exemplo escutando na porta 3000!");
 });
